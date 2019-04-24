ee99f3133c45f0334376784714b5cfc32a1536f3
Yuan Wang <gitlab@yuanagain.com>
2019-04-24 01:25:24 -0500

    github update

---
+++ precache-manifest.40cda2a197033be9ce5c79578f952a57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a21981936e84f5ec01907ead0e9a5b49",
+    "url": "/index.html"
+  },
+  {
+    "revision": "15d6b2b5d08224e0ea75",
+    "url": "/static/css/2.2acdec08.chunk.css"
+  },
+  {
+    "revision": "df1c4b80802b978b0444",
+    "url": "/static/css/main.5e87e8be.chunk.css"
+  },
+  {
+    "revision": "15d6b2b5d08224e0ea75",
+    "url": "/static/js/2.656d67f6.chunk.js"
+  },
+  {
+    "revision": "df1c4b80802b978b0444",
+    "url": "/static/js/main.4388989c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
